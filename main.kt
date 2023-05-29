1b20342172802c3aa5e85d3242cef148174a16b9
MISAGITHAY <125407240+Rabelaqbv@users.noreply.github.com>
2023-05-29 17:55:29 -0300

    Add files via upload

---
+++ main.kt
@@ -0,0 +1,20 @@
+fun main() {
+  println("Olá Mundo!")
+  var name = "Minelli"
+  val nascimento = 2006
+  println(nome) // exibe o nome
+  println(nascimento) // exibe o nascimento
+
+  val myNum: Int = 5 // Int
+  val myDoubleNum: Double = 5.99 // Double
+  val myLetter: Char = 'D' // Char
+  val myBoolean: Boolean = true // Boolean
+  val myText: String = "Olá" // String
+
+  println(myNum)
+  println(myDoubleNum)
+  println(myLatter)
+  println(myBoolean)
+  println(myText)
+  
+}
